e2d344aa3a5aaa71586ff09c07ea7c00bd0d75a3
anilyavas <anil.yavas99@gmail.com>
2025-04-30 18:30:45 +0300

    Update result.tsx to display image and prompt details

--- app/result.tsx
+++ app/result.tsx
@@ -1,17 +1,37 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
-
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Entypo } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router, useLocalSearchParams } from 'expo-router';
+import { View, Text, Image, Pressable } from 'react-native';
 
 export default function Details() {
-  const { name } = useLocalSearchParams();
+  const { imageUrl, prompt, style } = useLocalSearchParams();
+
+  const url = Array.isArray(imageUrl) ? imageUrl[0] : imageUrl;
+  const userPrompt = Array.isArray(prompt) ? prompt[0] : prompt;
+  const selectedStyle = Array.isArray(style) ? style[0] : style;
 
   return (
-    <>
-      <Stack.Screen options={{ title: 'Details' }} />
-      <Container>
-        <ScreenContent path="screens/details.tsx" title={`Showing details for user ${name}`} />
-      </Container>
-    </>
+    <View className="flex-1">
+      <LinearGradient style={{ flex: 1 }} colors={['#09090B', '#1C55FF', '#C26CFF', '#09090B']}>
+        <View className="m-4 items-center justify-between">
+          <Text className="text-2xl font-extrabold text-[#FAFAFA]">Your Design</Text>
+          <Pressable onPress={() => router.back()}>
+            <Entypo name="cross" size={20} color="#FAFAFA" />
+          </Pressable>
+        </View>
+        <View className="m-4">
+          <Image
+            source={{ uri: url }}
+            style={{ width: '100%', height: 300, borderRadius: 16 }}
+            resizeMode="contain"
+          />
+        </View>
+        <View className="m-4 gap-2 rounded-md bg-[#27272A]">
+          <Text className="text-md font-bold text-[#FAFAFA]/50">Prompt</Text>
+          <Text className="text-lg text-[#FAFAFA]">{userPrompt}</Text>
+          <Text className="text-sm text-[#FAFAFA]/50">{selectedStyle}</Text>
+        </View>
+      </LinearGradient>
+    </View>
   );
 }
